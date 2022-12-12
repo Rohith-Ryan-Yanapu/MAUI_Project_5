451a36313b14f3dbd098a06b2057e39481728294
Jitender <maheshwaramjitender444@gmail.com>
2022-12-11 19:30:59 -0600

    Added UI and Logic to AccountsList Page

--- MAUI_Project_5/MAUI_Project_5/Views/AccountsList.xaml.cs
+++ MAUI_Project_5/MAUI_Project_5/Views/AccountsList.xaml.cs
@@ -1,9 +1,48 @@
-namespace MAUI_Project_5.Views;
+﻿using System.Collections.ObjectModel;
+using PrivatePocketSafe.Data;
+using PrivatePocketSafe.Models;
+
+namespace PrivatePocketSafe.Views;
 
 public partial class AccountsList : ContentPage
 {
-	public AccountsList()
+    UNPItemDatabase database;
+    public ObservableCollection<UNPItem> Items { get; set; } = new();
+    public AccountsList(UNPItemDatabase Database)
 	{
 		InitializeComponent();
-	}
+        database = Database;
+        BindingContext = this;
+    }
+
+    protected override async void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        base.OnNavigatedTo(args);
+        var items = await database.GetItemsAsync();
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            Items.Clear();
+            foreach (var item in items)
+                Items.Add(item);
+        });
+    }
+    async void OnItemAdded(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(AccountDetails), true, new Dictionary<string, object>
+        {
+            ["Item"] = new UNPItem()
+        });
+    }
+
+    private async void  CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (e.CurrentSelection.FirstOrDefault() is not UNPItem item)
+            return;
+
+        await Shell.Current.GoToAsync(nameof(AccountDetails), true, new Dictionary<string, object>
+        {
+            ["Item"] = item
+        });
+    }
 }
+
