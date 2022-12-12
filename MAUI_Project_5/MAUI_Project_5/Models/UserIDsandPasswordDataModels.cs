bd4044aaddb3c68819da1623c621fd5a09664f6b
prudhvireddymali <prudhvireddymali2021@gmail.com>
2022-12-11 19:47:48 -0600

    Added Model for Database

---
+++ MAUI_Project_5/MAUI_Project_5/Models/UserIDsandPasswordDataModels.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace PrivatePocketSafe.Models;
+
+public class UNPItem
+{
+    [PrimaryKey, AutoIncrement]
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public string UserID { get; set; }
+    public string EmailAddress { get; set; }
+    public string Password { get; set; }
+    public string Description { get; set; }
+}
